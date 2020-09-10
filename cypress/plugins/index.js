bbbab1b6290b5bfe52d43f097c7fef2afb038563
Sebastian Hoß <sebhoss@pm.me>
2020-09-10 15:41:50 +0200

    disable gpu rendering for chrome based browsers

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,11 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
-    on('file:preprocessor', cucumber())
+    on('file:preprocessor', cucumber());
+    on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+            launchOptions.args.push('--disable-gpu');
+            return launchOptions
+        }
+    });
 }
